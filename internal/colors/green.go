9142468e9bc91db15fd060060f55324f46be64c8
sakitibi <sakitibi10nin@icloud.com>
2025-08-18 18:05:42 +0900

    Merge remote changes

--- internal/colors/green.go
+++ internal/colors/green.go
@@ -2,14 +2,14 @@ package colors
 
 import "github.com/fatih/color"
 
-func GreenPrint(content string, args1 any, args2 any) {
-	color.New(color.FgGreen, color.Bold).Println(content, args1, args2)
+func GreenPrint(format string, args ...any) {
+	color.New(color.FgGreen, color.Bold).Printf(format, args...)
 }
 
 func GreenPrint1(content string, args1 any) {
-	color.New(color.FgGreen, color.Bold).Println(content, args1)
+	color.New(color.FgGreen, color.Bold).Printf(content, args1)
 }
 
 func GreenPrintText(content string) {
-	color.New(color.FgGreen, color.Bold).Println(content)
+	color.New(color.FgGreen, color.Bold).Printf(content)
 }
--- internal/colors/red.go
+++ internal/colors/red.go
@@ -3,13 +3,13 @@ package colors
 import "github.com/fatih/color"
 
 func RedPrint(content string, args1 any, args2 any) {
-	color.New(color.FgRed, color.Bold).Println(content, args1, args2)
+	color.New(color.FgRed, color.Bold).Printf(content, args1, args2)
 }
 
 func RedPrint1(content string, args1 any) {
-	color.New(color.FgRed, color.Bold).Println(content, args1)
+	color.New(color.FgRed, color.Bold).Printf(content, args1)
 }
 
 func RedPrintText(content string) {
-	color.New(color.FgRed, color.Bold).Println(content)
+	color.New(color.FgRed, color.Bold).Printf(content)
 }
